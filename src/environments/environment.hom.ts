073c6546944a1176ff28f4f5cadfede122b945e2
Giovanni Silva <git@gsilva.pro>
2024-04-21 03:21:48 -0300

    fix wss connection

--- src/environments/environment.hom.ts
+++ src/environments/environment.hom.ts
@@ -1,4 +1,4 @@
 export const environment = {
   BACKEND_URL: "https://guess-game-api-hom.gsilva.pro",
-  BACKEND_WS: "ws://guess-game-api-hom.gsilva.pro"
+  BACKEND_WS: "wss://guess-game-api-hom.gsilva.pro"
 };
